0ee7dc93cc1444ed1c7e050829f1c5cd15a6d403
kukkunish <31717744+kukkunish@users.noreply.github.com>
2017-09-28 03:10:20 +0000

    Add files via upload

---
+++ usr.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import smtplib
+message="hi"
+s=smtplib.SMTP('smtp.gmail.com', 587)
+s.login("Nish9628@gmail.com", "006Nish#")
+s.sendmail("Nish9628@gmail.com","Nish9628@gmail.com",message)
+s.quit()
