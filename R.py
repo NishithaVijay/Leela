a8c74f4a82e32282ee031a02bc1fe4cb696601da
kukkunish <31717744+kukkunish@users.noreply.github.com>
2017-09-28 03:38:01 +0000

    Update R.py

--- R.py
+++ R.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import random
+import random #system choose the random numbers
 count=0
 r=0
 while count<=100:
-	roll=input("press r to roll the dice")
+	roll=input("press r to roll the dice") #r is used to roll the dice 
 	if roll=="r":
-		r=random.randint(1,6)
+		r=random.randint(1,6) #it gives a radom number between 1 & 6
 		count=count+r
 		print("your random num is",r)
 		print ("your on count",count)
