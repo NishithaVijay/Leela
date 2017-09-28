497d17d9056d24f87121f410bc6dd2d30cd95c91
kukkunish <31717744+kukkunish@users.noreply.github.com>
2017-09-28 03:55:31 +0000

    Delete user1.py

--- user1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import smtplib
-message = "hi"
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.login("Nish9628@gmail.com", "006Nish#")
-s.sendmail("Nish9628@gmail.com","Nish9628@gmail.com",message)
