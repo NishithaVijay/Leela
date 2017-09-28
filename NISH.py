df5f26c9bf071e78baf7b20d8c0d394ac9da81b5
kukkunish <31717744+kukkunish@users.noreply.github.com>
2017-09-28 03:34:10 +0000

    Update NISH.py

--- NISH.py
+++ NISH.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # this programes talks about dice
 import random
-r=random.randint(1,6) 
-print(r)
-if r==6:
+r=random.randint(1,6) #it gives random number between 1 & 6
+print(r) #output where a number is btwn 1 & 6
+if r==6: 
   print("congratulations you got 6")
